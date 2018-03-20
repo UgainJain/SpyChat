6bb9143601a27a4c0245192a622ede5f5345fad0
ugainjain <ugainjain@gmail.com>
2018-03-20 22:16:17 +0530

    SpyChat

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("What's your spy name??")
+Salutation = input("What would be your spy salutation, Mr. ,Mrs or Ms.")
+full_name = Salutation + " "+ name
+print("Alright " + full_name + ", I would like to know little more about you....")
