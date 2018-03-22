3eae7ce348006379274a8ef5c5c476915c7c87ce
ugainjain <ugainjain@gmail.com>
2018-03-22 22:50:34 +0530

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 name = input("What's your spy name??")
-Salutation = input("What would be your spy salutation, Mr. ,Mrs or Ms.")
-full_name = Salutation + " "+ name
-print("Alright " + full_name + ", I would like to know little more about you....")
+if len(name)>0 :
+    print("Yay, the name is good.")
+    Salutation = input("What would be your spy salutation, Mr. ,Mrs or Ms.")
+    full_name = Salutation + " " + name
+    print("Alright " + full_name + ", I would like to know little more about you....")
+    age = int( input("what's your age?" ))
+    if 20 < age < 50:
+        print("Alright,")
+        rating = float(input("whats ur Spy rating??"))
+        if 2.5 <= rating < 3.5:
+            print(" U can always do better")
+        elif 3.5 <= rating < 4.5:
+            print("Yup, you are one of good ones")
+        elif rating >= 4.5:
+            print("Ooo, thts an ace")
+        else:
+            print("We can always use somebody to help in the office.")
+        ol= bool(input("Are u online???"))
+        if ol != False:
+             print("Authentication complete, welcome" + Salutation + " " + name + " with age ")
+             print(repr(age) + " and rating of " + repr(rating) + " Proud to have u you on board")
+        else:
+            print("  ")
+    else:
+        print("Sorry you are not of the correct age to be a spy")
+else:
+    print("This name is not valid please try with a better name")
