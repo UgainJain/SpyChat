ad00efed7393e3eea41131e2cc224053ac4d9e47
ugainjain <ugainjain@gmail.com>
2018-03-29 22:41:59 +0530

    Fifth commit...

--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,8 @@
-name = "Ugain jain"
-salutation = "Mr."
-full_name = salutation + " "+ name
-age = 21
-rating = 3.8
-status = "Crazy me...."
+# name = "Ugain jain"
+# salutation = "Mr."
+# full_name = salutation + " "+ name
+# age = 21
+# rating = 3.8
+# status = "Crazy me...."
+
+spy = {"name": "Ugain", "Salutation": "Mr", "age": 21, "Rating": 3.8, "Status": "CRazy mee...."}
